f0b420251802216936672c76cff3b9368d186173
Ali-Rida Ammar <sam.onx@outlook.com>
2024-02-27 02:50:19 +0100

    Fixed the stars, added title with nice lavalamp style, no issues

--- client/src/Login.js
+++ client/src/Login.js
@@ -29,32 +29,52 @@ const AnimatedLoginForm = () => {
   );
 };
 
+const AnimatedTitle = () => {
+  const fade = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: 1000 }, // change this to the length of your animation
+  });
+
+  return (
+    <animated.h1 style={fade} className="title">VantaBlack Pro</animated.h1>
+  );
+};
+
 const Login = () => {
   const [showLogo, setShowLogo] = useState(true);
+  const [showStars, setShowStars] = useState(false);
 
   const handleLogoAnimationFinished = () => {
     setShowLogo(false);
+    setShowStars(true);
   };
 
-  const stars = [...Array(300)].map((_, i) => {
-    const size = Math.random() * 35; // vary size between 0 and 5
+  const stars = showStars && [...Array(300)].map((_, i) => {
+    const size = Math.random() * .8; // vary size between 0 and 2
     const opacity = Math.random(); // vary opacity between 0 and 1
     return (
-      <svg key={i} className="stars" style={{ width: `${size}px`, height: `${size}px`, opacity }} viewBox="0 0 10 10">
-        <polygon points="5,0 10,5 5,10 0,5" fill="white" />
+      <svg
+        key={i}
+        className="stars"
+        style={{ width: `${size}em`, height: `${size}em`, opacity, fill: 'white' }}
+        viewBox="0 0 50 50"
+      >
+        <polygon points="25,0 50,25 25,50 0,25" />
       </svg>
     );
   });
 
-
-
   return (
     <div className="login">
-        {stars}
+      {stars}
       {showLogo ? (
         <LogoAnimation onFinished={handleLogoAnimationFinished} />
       ) : (
-        <AnimatedLoginForm />
+        <>
+          <AnimatedTitle />
+          <AnimatedLoginForm />
+        </>
       )}
     </div>
   );
